c175c8e9fbbe94f6f2b7af55803be9f9e3df74b5
Phape <phape@icloud.com>
2019-03-13 11:39:09 +0100

    Initial commit

---
+++ src/Aufgabe_2/ButtonListener.java
@@ -0,0 +1,24 @@
+package Aufgabe_2;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+import static Aufgabe_2.TimeGui.chosenDateFormat;
+
+public class ButtonListener implements ActionListener {
+
+    private JLabel dateTextField;
+    private Date date;
+
+    public ButtonListener(JLabel dateTextField) {
+        this.dateTextField = dateTextField;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        date = new Date(System.currentTimeMillis());
+        dateTextField.setText(chosenDateFormat.format(date));
+    }
+}
