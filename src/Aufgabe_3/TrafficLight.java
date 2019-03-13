cec7cc223a9c679723f26b2c918bb77af8c9473b
Phape <phape@icloud.com>
2019-03-13 12:12:48 +0100

    added TrafficLight

---
+++ src/Aufgabe_3/TrafficLight.java
@@ -0,0 +1,25 @@
+package Aufgabe_3;
+
+import javax.swing.*;
+
+public class TrafficLight {
+
+    private JFrame frame;
+    private ButtonGroup buttonGroup;
+    private JRadioButton radioButton;
+
+    public TrafficLight() {
+        frame = new JFrame("Traffic light");
+        frame.setSize(500, 250);
+        buttonGroup = new ButtonGroup();
+        radioButton = new JRadioButton("TestButton");
+        frame.add(buttonGroup);
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        TrafficLight trafficLight = new TrafficLight();
+    }
+}
