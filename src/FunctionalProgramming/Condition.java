fe5bd3d44e87dd4b7c598218d0818f55ff896d76
Phape <phape@icloud.com>
2019-04-15 11:26:43 +0200

    added exercises from worksheet 2 (done in DH)

---
+++ src/FunctionalProgramming/Condition.java
@@ -0,0 +1,6 @@
+package FunctionalProgramming; //sheet 2 ex.2
+
+@FunctionalInterface
+public interface Condition {
+    boolean appliesTo (String s);
+}
---
+++ src/FunctionalProgramming/Verification.java
@@ -0,0 +1,7 @@
+package FunctionalProgramming; //sheet 2 ex. 1
+
+@FunctionalInterface
+public interface Verification {
+
+    boolean isSuccessfulFor (int i);
+}
