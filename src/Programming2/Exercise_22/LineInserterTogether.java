f7297443e731aedf94492686f2dc7ec57574898e
Phape <phape@icloud.com>
2019-05-02 16:11:32 +0200

    added exercise 22 and 23

---
+++ src/Programming2/Exercise_22/LineInserterTogether.java
@@ -0,0 +1,63 @@
+package Programming2.Exercise_22;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+ //I didn't finish copying
+public class LineInserterTogether {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a filename");
+        String name = scanner.nextLine();
+        System.out.println("Please enter the line number");
+        int n = scanner.nextInt();
+        System.out.println("Please enter the text to be inserted");
+        String lnNr = scanner.nextLine();
+
+
+        try {
+            FileReader fileReader = new FileReader(name);
+            BufferedReader buffedReader = new BufferedReader(fileReader);
+
+            PrintWriter printWriter = new PrintWriter("BlaBla.txt");
+
+            String s = buffedReader.readLine();
+            int i = 1;
+            while (i < n && s != null) {
+                printWriter.println(s);
+                s = buffedReader.readLine();
+                i++;
+            }
+            printWriter.println(lnNr);
+
+            while (s != null) {
+                printWriter.println(s);
+                s = buffedReader.readLine();
+            }
+
+            printWriter.close();
+            buffedReader.close();
+            fileReader.close();
+
+            fileReader = new FileReader("Blabla.txt");
+            buffedReader = new BufferedReader(fileReader);
+            printWriter = new PrintWriter(name);
+
+            s = buffedReader.readLine();
+
+            while (s != null) {
+                printWriter.println(s);
+                s = buffedReader.readLine();
+            }
+
+            printWriter.close();
+            buffedReader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Programming2/Exercise_23/TokenBasedIO.java
@@ -0,0 +1,8 @@
+package Programming2.Exercise_23;
+
+public class TokenBasedIO {
+
+    public static void main(String[] args) {
+
+    }
+}
